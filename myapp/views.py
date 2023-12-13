2f0e5b31df5c16d20e78d394f89ea1cacaf9308b
Sandr89 <oleksandr.khomiak@gmail.com>
2023-12-13 03:31:29 +0200

    2st

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def hello_world(request, variant):
+    response_data = {'message': f'Hello World {variant}'}
+    return JsonResponse(response_data, status=200)
